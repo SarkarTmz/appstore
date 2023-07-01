6063a5cb6453f01b95239b905dcd768ba5f906c6
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-07-01 14:37:47 +0545

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/utils.css';
+import '../styles/components/header.css';
+import '../styles/components/profile.css';
+import '../styles/components/about.css';
+import '../styles/components/service.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+
+
+import mobileNav from './utils/mobile-nav';
+mobileNav();
